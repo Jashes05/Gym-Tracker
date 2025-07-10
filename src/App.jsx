2ae840e65f2110aeaaf9133867c9ae6eea4649f2
Jashes05 <ashishjoshi2390@gmail.com>
2025-07-10 08:58:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PushPullTracker from "./PushPullTracker";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', color: 'white', marginTop: '10px' }}>🏋️ Push Pull Gym Tracker</h1>
+      <PushPullTracker />
+    </div>
+  );
+}
+
+export default App;
